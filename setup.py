108e2e0a3913403dab54f24e19afa800f75d45dc
vidito <niamadpour@gmail.com>
2024-12-29 17:49:37 +0100

    little improvements to setup file

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ setup(
     version="0.1.1",
     packages=find_packages(),
     install_requires=[
-        "spacy",
-        "scikit-learn",
-        "pandas",
-        "numpy",
-        "vaderSentiment"
+        "spacy>=3.0.0,<4.0.0",
+        "scikit-learn>=0.24.0,<2.0.0",
+        "pandas>=1.0.0,<2.0.0",
+        "numpy>=1.19.0,<2.0.0",
+        "vaderSentiment>=3.3.2,<4.0.0",
     ],
     package_data={
+        "vidinlp": ["data/*.txt"],  # Include all .txt files in the `data` folder
     },
     author="Vahid Niamadpour",
     author_email="contact@pythonology.eu",
     description="A simple, modern, and fast NLP library built on top of spaCy, gensim, vadersentiment.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/vidito/vidinlp",
     classifiers=[
